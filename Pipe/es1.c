a3ddb5249dba704780c2945891d82a8603ac6c2e
Diego Renesto <diego.renesto@iisviolamarchesini.edu.it>
2025-03-17 20:04:40 +0100

    esercizi programmazione-di-sistema

---
+++ Pipe/es1.c
@@ -0,0 +1,57 @@
+/* Scrivere un programma concorrente che realizzi il seguente comando:
+
+cat costituzione.txt | more
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+    // Controllo argomenti
+    if (argc != 2)
+    {
+        perror("Numero argomenti sbagliato!\n");
+        exit(1);
+    }
+
+    // Dichiarazione variabili
+    pid_t pid;
+    int p1p0[2];
+
+    // Creazione pipe
+    pipe(p1p0);
+
+    if (pipe(p1p0) == -1)
+    {
+        perror("Errore creazione pipe!\n");
+        exit(2);
+    }
+
+    pid = fork(); // creo figlio
+
+    if (pid == 0) // figlio
+    {
+        close(p1p0[0]);                               // chiudo lettura
+        execl("/bin/cat", "cat", argv[1], (char *)0); // eseguo cat
+        return -1;
+    }
+
+    pid = fork();
+
+    if (pid == 0)
+    {
+        close(p1p0[1]);
+        execl("/bin/more", "more", (char *)0);
+        return -1;
+    }
+
+    close(p1p0[0]); // chiudo lettura
+    close(p1p0[1]); // chiudo scrittura
+    wait(&pid);     // aspetto che i figli terminino
+    wait(&pid);
+
+    return 0;
+}
